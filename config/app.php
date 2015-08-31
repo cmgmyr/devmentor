4faf8bae430617c57fd9e19af87f1af01be2aa4d
Chris Gmyr <cmgmyr@gmail.com>
2015-08-31 18:25:21 -0400

    add provider and env values

--- config/app.php
+++ config/app.php
@@ -138,6 +138,11 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Other Service Providers
+         */
+        Laravel\Socialite\SocialiteServiceProvider::class,
+
+        /*
          * Application Service Providers...
          */
         DevMentor\Providers\AppServiceProvider::class,
@@ -191,6 +196,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Socialize' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
